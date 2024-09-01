c89cd14e33f1bf07edd850db036a80cf3c0d4e81
Sandesh <sandeshmadoori@gmail.com>
2024-09-01 23:26:17 +0530

    cors config

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+
+app.use(cors({
+  origin: 'https://stockmanagementsystem-client.vercel.app'
+}))
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
