f959b2850399fff47d4dc55894e105e1930f429b
Sandesh <sandeshmadoori@gmail.com>
2024-09-01 23:52:45 +0530

    mongodb

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
